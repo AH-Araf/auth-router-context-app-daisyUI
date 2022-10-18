381ca877fdcf1486f509a195a40fac5d1c189ecf
Araf <arafathossain.baiust@gmail.com>
2022-10-19 02:04:13 +0600

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React, { useContext } from 'react';
+import { AuthContext } from '../context/UserContext';
+
+const Home = () => {
+    const {user} = useContext(AuthContext)
+    return (
+        <div>
+            <h1>Home for {user?.displayName}</h1>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAxxvWBwdttGVi5YIp30mat_KUQSRlAaqU",
+  authDomain: "auth-router-context-79cc1.firebaseapp.com",
+  projectId: "auth-router-context-79cc1",
+  storageBucket: "auth-router-context-79cc1.appspot.com",
+  messagingSenderId: "494813693277",
+  appId: "1:494813693277:web:6f6b26b721ac36400ed4b4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
